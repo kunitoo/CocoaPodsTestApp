ced999cced70aaf21ed99c5fdabe5de5c4574a78
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2015-09-16 16:08:09 +0900

    Initial Commit

---
+++ CocoaPodsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPodsTestApp
+//
+//  Created by Takashi Hatakeyama on 2015/09/16.
+//  Copyright (c) 2015年 esminc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
